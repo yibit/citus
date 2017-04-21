53ae15b2859dc242a6bff5f76ebd9acfd3e6963f
Jason Petersen <jason@citusdata.com>
2017-04-20 20:56:35 -0600

    Add pg_regress_multi support for PostgreSQL 10

--- src/test/regress/pg_regress_multi.pl
+++ src/test/regress/pg_regress_multi.pl
@@ -424,7 +424,7 @@ my @arguments = (
     '--user', $user
 );
 
-if ($majorversion eq '9.5' || $majorversion eq '9.6')
+if ($majorversion eq '9.5' || $majorversion eq '9.6' || $majorversion eq '10')
 {
     push(@arguments, '--bindir', "tmp_check/tmp-bin");
 }
