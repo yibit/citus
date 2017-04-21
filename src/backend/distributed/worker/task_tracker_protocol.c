616e78b44a3b08aa62443fe05eeb3f31467ae4d7
Jason Petersen <jason@citusdata.com>
2017-04-20 19:16:55 -0600

    Remove Node cast of authrole (now RoleSpec)

--- src/backend/distributed/worker/task_tracker_protocol.c
+++ src/backend/distributed/worker/task_tracker_protocol.c
@@ -280,13 +280,14 @@ CreateJobSchema(StringInfo schemaName)
 
 	createSchemaStmt = makeNode(CreateSchemaStmt);
 	createSchemaStmt->schemaname = schemaName->data;
-	createSchemaStmt->authrole = (Node *) &currentUserRole;
 	createSchemaStmt->schemaElts = NIL;
 
 	/* actually create schema with the current user as owner */
 #if (PG_VERSION_NUM >= 100000 && PG_VERSION_NUM < 110000)
+	createSchemaStmt->authrole = &currentUserRole;
 	CreateSchemaCommand(createSchemaStmt, queryString, -1, -1);
 #else
+	createSchemaStmt->authrole = (Node *) &currentUserRole;
 	CreateSchemaCommand(createSchemaStmt, queryString);
 #endif
 
